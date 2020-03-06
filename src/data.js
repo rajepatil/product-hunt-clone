dca8f11e7391a0455028988fe49912f948d5bfca
Rajesh mule <rajeshsmule@gmail.com>
2020-03-06 11:32:22 +0530

    make a data file separet

---
+++ src/data.js
@@ -0,0 +1,17 @@
+
+export const names = [{ name: "Open Peeps", description: "A hand-drawn illustration library.", tags: ["Design Tools", "productivity"], src: "https://ph-files.imgix.net/719fe138-8598-46ec-8259-ef17ba46c0c2?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop", up: 1233, count: 222 },
+{ name: "InVideo", description: "Create insanely good social videos in under 10 mins", tags: ["Design Tools", "productivity"], src: "https://ph-files.imgix.net/9093cadf-bc92-45d3-97ea-a74d41455587?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop", up: 2323, count: 242 },
+{ name: "Phone Call Translator", description: "Translates your voice calls into 29 languages in real-time", tags: ["Android", "iphone"], src: "https://ph-files.imgix.net/76ee2898-6c6c-4d57-86c3-f42b961a97dc?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop", up: 4343, count: 221 }]
+
+export const profileSrc = [{ imgsrc: "https://ph-avatars.imgix.net/1456069/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/826276/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/1440175/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/2422879/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/166755/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/1903430/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/2273447/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/17305/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/1126875/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/2097425/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/2245194/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" },
+{ imgsrc: "https://ph-avatars.imgix.net/2395344/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=30&h=30&fit=crop" }]
