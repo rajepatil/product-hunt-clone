bfe01d2f000dc10ff889c2d080a55180162dab97
Rajesh mule <rajeshsmule@gmail.com>
2020-03-10 16:09:10 +0530

    Updates

---
+++ precache-manifest.edf5227826eaec9bbf309b6d3f040ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cffe1361fd6230a356ec099098cf491",
+    "url": "/product-hunt-clone/index.html"
+  },
+  {
+    "revision": "01ba79034bcf1ada1604",
+    "url": "/product-hunt-clone/static/css/main.4daa40df.chunk.css"
+  },
+  {
+    "revision": "9436d90d560c21dda387",
+    "url": "/product-hunt-clone/static/js/2.2bd67f70.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/product-hunt-clone/static/js/2.2bd67f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ba79034bcf1ada1604",
+    "url": "/product-hunt-clone/static/js/main.701e3479.chunk.js"
+  },
+  {
+    "revision": "81404a47531a4e4109c3",
+    "url": "/product-hunt-clone/static/js/runtime-main.af105efe.js"
+  }
+]);
