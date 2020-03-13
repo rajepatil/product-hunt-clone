715aee7e7e2f16a92345743bf5e887af2ab312aa
Rajesh mule <rajeshsmule@gmail.com>
2020-03-13 05:47:52 +0530

    priter

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,41 @@
-import React from 'react';
-import './App.css';
-import { Cards } from './Cards';
-import { Header } from './header';
-import { Asidecard } from './Asidecard';
+import React from "react";
+import "./App.css";
+import { Cards } from "./Cards";
+import { Header } from "./header";
+import { Asidecard } from "./Asidecard";
 
+import { profileSrc } from "../data";
 
-import { profileSrc } from '../data';
-
-class App extends React.Component
-{
-
-  constructor(props)
-  {
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      posts:[]
+      posts: []
     };
   }
 
   componentDidMount() {
-    fetch("https://api.producthunt.com/v1/posts/",{
+    fetch("https://api.producthunt.com/v1/posts/", {
       method: "GET",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
-        "Authorization": "Bearer 0ns-dkfbamLRViMA-xC1CmcaRFBPB_Zoteoweo4Z7Cc",
-        "Host": "api.producthunt.com"
+        Authorization: "Bearer 0ns-dkfbamLRViMA-xC1CmcaRFBPB_Zoteoweo4Z7Cc",
+        Host: "api.producthunt.com"
       }
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data.posts)
-      this.setState({
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.posts);
+        this.setState({
           posts: data.posts
-        }) 
-    })
-    .catch(error => console.log(error));
-        
+        });
+      })
+      .catch(error => console.log(error));
   }
 
-
-  updateCount = votes_count =>
-  {
-    let updatedData = this.state.posts.map(post =>
-    {
+  updateCount = votes_count => {
+    let updatedData = this.state.posts.map(post => {
       if (post.votes_count === votes_count) {
         post.votes_count = post.votes_count + 1;
       }
@@ -52,14 +44,17 @@ class App extends React.Component
     this.setState({ posts: updatedData });
   };
 
-  render()
-  {
+  render() {
     return (
       <div className="App">
         <Header />
         <div className="container flex">
           {/*<Cards names={names} updateCount={this.updateCount} title="Popular this month" />*/}
-          <Cards posts={this.state.posts} updateCount={this.updateCount} title="Popular this month" />
+          <Cards
+            posts={this.state.posts}
+            updateCount={this.updateCount}
+            title="Popular this month"
+          />
 
           <Asidecard title="Makers working today" profileSrc={profileSrc} />
         </div>
@@ -69,5 +64,3 @@ class App extends React.Component
 }
 
 export default App;
-
-
